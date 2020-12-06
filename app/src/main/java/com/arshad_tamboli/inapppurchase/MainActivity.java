0cfa42cc4c1d7f7b97d4590b5bac9b3140ed3f6e
Arshad Tamboli <arshadtamboli@rapidera.com>
2020-12-06 17:06:24 +0530

    Initial Commit

---
+++ app/src/main/java/com/arshad_tamboli/inapppurchase/MainActivity.java
@@ -0,0 +1,14 @@
+    package com.arshad_tamboli.inapppurchase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
