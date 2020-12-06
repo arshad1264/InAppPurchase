a262efa24269d1b91bab91b4dffe34c15ef33124
Arshad Tamboli <arshadtamboli@rapidera.com>
2020-12-06 19:11:10 +0530

    Integrate In App Purchase

--- app/src/main/java/com/arshad_tamboli/inapppurchase/MainActivity.java
+++ app/src/main/java/com/arshad_tamboli/inapppurchase/MainActivity.java
@@ -1,14 +1,63 @@
-    package com.arshad_tamboli.inapppurchase;
+package com.arshad_tamboli.inapppurchase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.anjlab.android.iab.v3.BillingProcessor;
+import com.anjlab.android.iab.v3.TransactionDetails;
+
+public class MainActivity extends AppCompatActivity implements BillingProcessor.IBillingHandler {
+
+    BillingProcessor bp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bp = new BillingProcessor(this, "YOUR LICENSE KEY FROM GOOGLE PLAY CONSOLE HERE", this);
+        bp.initialize();
+
+        Button buttonPayNow = findViewById(R.id.buttonPayNow);
+        buttonPayNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bp.purchase(MainActivity.this, "YOUR PRODUCT ID FROM GOOGLE PLAY CONSOLE HERE");
+            }
+        });
+
     }
+
+    @Override
+    public void onProductPurchased(String productId, TransactionDetails details) {
+
+    }
+
+    @Override
+    public void onPurchaseHistoryRestored() {
+
+    }
+
+    @Override
+    public void onBillingError(int errorCode, Throwable error) {
+
+    }
+
+    @Override
+    public void onBillingInitialized() {
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (!bp.handleActivityResult(requestCode, resultCode, data)) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+
 }
