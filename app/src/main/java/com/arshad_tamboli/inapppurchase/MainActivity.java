a914a840167552e6f331673f01af2b2e4ad3a349
Arshad Tamboli <arshadtamboli@rapidera.com>
2020-12-07 10:26:53 +0530

    Print Logs

--- app/src/main/java/com/arshad_tamboli/inapppurchase/MainActivity.java
+++ app/src/main/java/com/arshad_tamboli/inapppurchase/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -12,21 +13,21 @@ import com.anjlab.android.iab.v3.TransactionDetails;
 
 public class MainActivity extends AppCompatActivity implements BillingProcessor.IBillingHandler {
 
-    BillingProcessor bp;
+    private BillingProcessor billingProcessor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bp = new BillingProcessor(this, "YOUR LICENSE KEY FROM GOOGLE PLAY CONSOLE HERE", this);
-        bp.initialize();
+        billingProcessor = new BillingProcessor(this, "YOUR LICENSE KEY FROM GOOGLE PLAY CONSOLE HERE", this);
+        billingProcessor.initialize();
 
         Button buttonPayNow = findViewById(R.id.buttonPayNow);
         buttonPayNow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bp.purchase(MainActivity.this, "YOUR PRODUCT ID FROM GOOGLE PLAY CONSOLE HERE");
+                billingProcessor.purchase(MainActivity.this, "android.test.purchased");
             }
         });
 
@@ -34,27 +35,28 @@ public class MainActivity extends AppCompatActivity implements BillingProcessor.
 
     @Override
     public void onProductPurchased(String productId, TransactionDetails details) {
-
+        Log.e(getClass().getSimpleName(), "onProductPurchased");
+        Log.e(getClass().getSimpleName(), "Product Id " + productId);
     }
 
     @Override
     public void onPurchaseHistoryRestored() {
-
+        Log.e(getClass().getSimpleName(), "onPurchaseHistoryRestored");
     }
 
     @Override
     public void onBillingError(int errorCode, Throwable error) {
-
+        Log.e(getClass().getSimpleName(), "onBillingError");
     }
 
     @Override
     public void onBillingInitialized() {
-
+        Log.e(getClass().getSimpleName(), "onBillingInitialized");
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (!bp.handleActivityResult(requestCode, resultCode, data)) {
+        if (!billingProcessor.handleActivityResult(requestCode, resultCode, data)) {
             super.onActivityResult(requestCode, resultCode, data);
         }
     }
